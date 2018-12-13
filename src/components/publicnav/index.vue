b6956d7ac08daa8c2b8e2201abe8559d9a618919
Mia <952481706@qq.com>
2018-12-13 18:06:27 +0800

    apex blocks

--- src/components/publicnav/index.vue
+++ src/components/publicnav/index.vue
@@ -3,21 +3,26 @@
     <div class="main clearboth">
       <div class="logo fl">
         <a href="/home">
-          <img src="../../assets/images/shared/logo.png" alt=""></a>
+          <img src="../../assets/images/logo.png" alt=""  ></a>
       </div>
-      <ul class="fl">
-        <li v-for="(item, index) in nav" :key="index" :class=" ((item.path === defaultNav) || ( defaultNav === '/' &&  index == '0')) ? 'active' : ''">
-          <router-link :to="item.path">{{item.title}}</router-link>
-        </li>
-      </ul>
-      <div class="search-box fr clearboth">
-        <input class="fl" type="text" placeholder=" e.g.  Account  /  Transaction  /  Block number">
-        <div class="fl"></div>
+      <div class="search-box fl clearboth">
+        <input class="fl"  type="text"  placeholder="Search Accounts/Transactions/Blocks"  maxlength="100" >
+        <div class="fl search-btn"></div>
       </div>
-      <div class="language">
-        <span @click="changeLangEvent">{{$i18n.locale}}</span>
+      <div
+        class="language" :class="isShow ? 'active' : ''" @click="changeLang" >
+        <i></i>
+        <span class="language-span" v-show="isShow" @click.stop="changeLangEvent" >{{language}}</span>
       </div>
     </div>
+    <div class="nav-bar">
+      <ul class="fl">
+        <li v-for="(item, index) in nav" :key="index" :class=" ((item.path === defaultNav) || ( defaultNav === '/' &&  index == '0')) ? 'active' : ''">
+          <router-link :to="item.path"></router-link>
+        </li>
+      </ul>
+    </div>
+
 
   </div>
 </template>
@@ -27,30 +32,49 @@ export default {
   props: ["active"],
   data() {
     return {
-      navIndex: 0
+      navIndex: 0,
+      isShow: false,
+      language: "中文"
     };
   },
   computed: {
     nav() {
       return [
         { title: this.$t("nav.home"), path: "/home" },
-        { title: this.$t("nav.wallet"), path: "/wallet" }
-        // { title: this.$t("nav.producer"), path: "/producer" }
+        { title: this.$t("nav.wallet"), path: "/wallet" },
+        { title: '', path: "/blocks" },
+        { title: '', path: "/transactions" },
+        { title: '', path: "/producer" },
       ];
     },
     defaultNav() {
       return "/" + this.$route.path.split("/")[1];
     }
   },
+  mounted() {
+    let _this = this;
+    document.addEventListener("click", function(e) {
+      // let flag = e.target.contains(document.getElementsByClassName("language-span")[0]);
+      let flag = e.target.className == "language"
+      if (flag) return;
+      _this.isShow = false;
+    });
+  },
   methods: {
     // checkRouterLocal(path) {
     //   this.navIndex = this.nav.findIndex(item => item.path === path);
     // },
+    changeLang() {
+      this.isShow = !this.isShow;
+    },
     changeLangEvent() {
+      console.log(this.$i18n.locale);
       if (this.$i18n.locale === "ZH") {
+        this.language = "English";
         localStorage.setItem("locale", "EN");
         this.$i18n.locale = localStorage.getItem("locale");
       } else {
+        this.language = "中文";
         localStorage.setItem("locale", "ZH");
         this.$i18n.locale = localStorage.getItem("locale");
       }
@@ -65,72 +89,137 @@ export default {
   top: 0;
   left: 0;
   width: 100%;
-  height: 60px;
-  background: #00c1ff;
-  border-bottom: 1px dashed #3699ff;
+  height: 90px;
   .main {
     position: relative;
   }
   .logo {
     img {
-      margin-top: 16px;
+      margin-top: 34px;
     }
   }
-  ul {
-    margin-left: 60px;
-    padding-top: 16px;
-    li {
-      float: left;
-      padding: 0 22px;
-      a {
-        line-height: 26px;
-        color: #fff;
-        opacity: 0.6;
-        -moz-transition: all 0.3s;
-        -o-transition: all 0.3s;
-        -webkit-transition: all 0.3s;
-      }
-      &:hover,
-      &.active {
-        a{
-          opacity: 1;
-        }
-      }
-    }
-  }
+ 
   .search-box {
-    padding-top: 14px;
-    height: 48px;
+    padding-top: 26px;
+    height: 56px;
     position: relative;
+    margin-left: 20px;
     input {
-      width: 400px;
+      width: 710px;
       height: 100%;
       border: 0px;
-      padding: 4px 56px 4px 6px;
+      border-radius: 4px;
+      font-size: 14px;
+      padding: 4px 56px 4px 15px;
+      background: rgba(255, 255, 255, 0.2);
+      color: #fff;
     }
-    div.fl {
+    .search-btn {
       position: absolute;
       right: 0;
       cursor: pointer;
       width: 50px;
-      height: 34px;
-      background:  url(../../assets/images/shared/search.png) center 6px no-repeat;
+      height: 30px;
+      background: url(../../assets/images/shared/search.png) center 6px
+        no-repeat;
     }
   }
   .language {
     position: absolute;
-    top: 16px;
-    left: calc(~"100% + 36px");
+    top: 25px;
+    right: -55px;
     line-height: 32px;
+    height: 32px;
     color: #fff;
-    width: 45px;
+    width: 100px;
+    cursor: pointer;
+    background: url(../../assets/images/shared/lang.png) left center no-repeat;
+    i {
+      position: absolute;
+      right: 0;
+      top: 0;
+      display: block;
+      width: 20px;
+      height: 100%;
+      background: url(../../assets/images/shared/icon-fix.png) no-repeat;
+      background-position: center -75px;
+      // transition: all .3s;
+    }
     span {
+      position: absolute;
+      top: 32px;
       display: block;
       width: 100%;
-      padding-left: 20px;
       box-sizing: border-box;
-      background: url(../../assets/images/shared/lang.png) left center no-repeat;
+      height: 32px;
+      text-align: center;
       cursor: pointer;
+    }
+    &.active {
+      i {
+        transform: rotate(-180deg);
+      }
+    }
+  }
+  .nav-bar{
+    z-index: 9999;
+    position: fixed;
+    bottom: 200px;
+    left:106px;
+    width: 30px;
+    height: 350px;
+    ul {
+      li {
+        width: 30px;
+        height: 30px;
+        margin-bottom: 25px;
+        background: url(../../assets/images/nav-fix.png)no-repeat;
+        background-position: center 3px;
+        a {
+          display: block;
+          width: 100%;
+          height: 100%;
+        }
+        &:hover,
+        &.active {
+            background-position: center -34px;
+        }
+        &:nth-child(2){
+          background-position: center -68px;
+          &:hover,
+          &.active {
+            background-position: center -99px;
+          }
+        }
+        &:nth-child(3){
+          background-position: center -134px;
+          &:hover,
+          &.active {
+            background-position: center -171px;
+          }
+        }
+        &:nth-child(4){
+          background-position: center -209px;
+          &:hover,
+          &.active {
+            background-position: center -246px;
+          }
+        }
+        &:nth-child(5){
+          background-position: center -283px;
+          &:hover,
+          &.active {
+            background-position: center -320px;
+          }
+        }
+      }
+    }
+  }
+}
+@media screen and(max-width:1366px) {
+  .nav{
+     .nav-bar{
+      left:55px;
     }
   }
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,334 +1,78 @@
 <template>
   <div class="home clearboth">
-    <div class="block apex-modul fl">
-      <p class="apex-title">Block</p>
-      <ul class="apex-list">
-        <vue-scroll :ops="ops">
+    <div class="top-modul clearboth">
+      <div class="overview apex-modul fl">
+        <p class="apex-title">Overview</p>
+        <ul class="clearboth">
           <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
+            <p>Last Block</p>
+            <p>1935566</p>
           </li>
           <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
+            <p>Max TPS</p>
+            <p>463</p>
           </li>
           <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
+            <p>Daily TX</p>
+            <p>1544945</p>
           </li>
           <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
+            <p>Daily Active Account</p>
+            <p>46</p>
           </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div>
-              <div class="top">
-                <p>Block 255,892</p>
-                <span>1,644 Bytes</span>
-              </div>
-              <div class="bottom">
-                <a href="javascript:;">APMa9Z3pRKrFDZwDvcnTm5vgbT1a3SyYFZe</a>
-                <span>2 seconds ago</span>
-              </div>
-            </div>
-          </li>
-        </vue-scroll>
-      </ul>
+        </ul>
+      </div>
+      <div class="tps apex-modul fr">
+        <p class="apex-title">Live TPS</p>
+        <div class="chart-box">
+          <div class="chart" id="echartContainer" style="width:100%;"></div>
+        </div>
+      </div>
     </div>
-    <div class="transactions apex-modul fr">
-      <p class="apex-title">Transactions</p>
-      <ul class="apex-list">
-        <vue-scroll :ops="ops">
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-          <li>
-            <div class="bottom">
-              <a href="javascript:;">1b26ff9f0ef2cca0a8b079a6423eef537edd03644982cf7</a>
-              <span>2 seconds ago</span>
-            </div>
-          </li>
-        </vue-scroll>
-      </ul>
+    <div class="bottom-modul clearboth">
+      <div class="block apex-modul fl">
+        <p class="apex-title">Blocks <span>ALL</span></p>
+        <ul class="apex-list">
+          <vue-scroll :ops="ops">
+            <li
+              v-for="(item,index) in blocks"
+              :key="index"
+            >
+              <div>
+                <div class="top">
+                  <p>Block {{item.height}}</p>
+                  <span>{{item.size}} Bytes</span>
+                </div>
+                <div class="bottom">
+                  <a href="javascript:;">{{item.hash}}</a>
+                  <span>{{item.age}}</span>
+                </div>
+              </div>
+            </li>
+          </vue-scroll>
+        </ul>
+      </div>
+      <div class="transactions apex-modul fr">
+        <p class="apex-title">Transactions<span>ALL</span></p>
+        <ul class="apex-list">
+          <vue-scroll :ops="ops">
+            <li
+              v-for="(item,index) in transactions"
+              :key="index"
+            >
+              <div class="bottom">
+                <a href="javascript:;">{{item.code}}</a>
+                <span>{{item.time}}</span>
+              </div>
+            </li>
+          </vue-scroll>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import echarts from "echarts";
 export default {
   name: "home",
   components: {},
@@ -339,42 +83,79 @@ export default {
         {
           height: 6353170,
           age: "13 secs ago",
-          txn: 61,
-          miner:
-            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25",
-          gasused: 7992394
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
         },
         {
           height: 6353170,
           age: "13 secs ago",
-          txn: 61,
-          miner:
-            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd26",
-          gasused: 7992394
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
         },
         {
           height: 6353170,
           age: "13 secs ago",
-          txn: 61,
-          miner:
-            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd27",
-          gasused: 7992394
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
         },
         {
           height: 6353170,
           age: "13 secs ago",
-          txn: 61,
-          miner:
-            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd28",
-          gasused: 7992394
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
         },
         {
           height: 6353170,
           age: "13 secs ago",
-          txn: 61,
-          miner:
-            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd29",
-          gasused: 7992394
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
+        },
+        {
+          height: 6353170,
+          age: "13 secs ago",
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
+        },
+        {
+          height: 6353170,
+          age: "13 secs ago",
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
+        },
+        {
+          height: 6353170,
+          age: "13 secs ago",
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
+        },
+        {
+          height: 6353170,
+          age: "13 secs ago",
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
+        },
+        {
+          height: 6353170,
+          age: "13 secs ago",
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
+        },
+        {
+          height: 6353170,
+          age: "13 secs ago",
+          size: 61,
+          hash:
+            "0x5ead841ac2c08e14ae45492ff3976160c3d7af7ae004cb557678df4bfcaacd25"
         }
       ],
       transactions: [
@@ -412,7 +193,100 @@ export default {
     };
   },
   created: function() {},
-  mounted() {},
+  mounted() {
+     // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById("echartContainer"));
+    // 绘制图表
+    myChart.setOption({
+      color: ["#1AC8FF"],
+      grid: {
+        left: 10,
+        right: 30,
+        top: 40,
+        bottom: 10,
+        containLabel: true
+      },
+      xAxis: {
+        type: "category",
+        boundaryGap :false,
+        data: [
+          "10:08:00",
+          "10:08:10",
+          "10:08:20",
+          "10:08:30",
+          "10:08:40",
+          "10:08:50",
+          "10:09:00",
+          "10:09:10",
+          "10:09:20"
+        ],
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "#999"
+          }
+        },
+      },
+      yAxis: {
+        type: "value",
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "#999"
+          }
+        },
+        splitLine: {
+          show: false
+        }      
+      },
+      tooltip: {
+        show: true,
+        trigger: "axis",
+        padding :  [8, 10],
+        backgroundColor :'rgba(161,161,161,0.1)',
+        axisPointer: {
+          type: "line",
+        },
+        textStyle:{
+          color: '#ffffff',
+          fontSize: '14px',
+        },
+        formatter: function (params, ticket, callback) {
+          return 'TPS '+ params[0].value;
+      }
+      },
+      series: [
+        {
+          name: "",
+          data: [100, 111, 102, 123, 105, 133, 155, 146, 129, 120],
+          type: "line",
+          smooth: true,
+          showSymbol: false,
+          symbol: 'circle',     //设定为实心点
+          symbolSize: 6,
+          itemStyle:{
+            color: 'rgba(242, 101, 34, .8)',
+          },
+          lineStyle:{
+            color: '#f26522'
+          },
+          areaStyle: {
+            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: "rgb(255,255,255,.2)"
+              },
+              {
+                offset: 1,
+                color: "rgb(255,255,255,0)"
+              }
+             
+            ])
+          }
+        }
+      ]
+    });
+  },
   methods: {},
   computed: {}
 };
@@ -421,5 +295,35 @@ export default {
 .home {
   width: 100%;
   height: 100%;
+  .bottom-modul,
+  .top-modul {
+    height: 49%;
+  }
+  .top-modul {
+    margin-bottom: 2%;
+  }
+  .tps{
+    .chart-box{
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      // height: 100%;
+      .chart{
+        height:300px;
+      }
+    }
+  }
+}
+@media screen and(max-width:1366px) {
+  .home{
+    .tps{
+      .chart-box{
+        .chart{
+          height: 190px;
+        }
+      }
+    }
+  }
 }
 </style>
